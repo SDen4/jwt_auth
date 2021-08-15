d3ceb432e084edc0c841b5cf48ec16b72b211117
SDen4 <asw325@yandex.ru>
2021-08-15 13:04:51 +0500

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -5,15 +5,26 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 
 // create an example of the app
 const app = express()
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 // start the app
 const start =  async() => {
   try {
+    // connect to db
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+
     app.listen(PORT, () => console.log(`Yahoo! Server started on port: ${PORT}.`))
   } catch (error) {
     console.log(error);
