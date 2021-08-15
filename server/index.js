ebf40d8fcf1786f87c89f90fef4e8000b0593457
SDen4 <asw325@yandex.ru>
2021-08-15 15:10:45 +0500

    user controller

--- server/index.js
+++ server/index.js
@@ -7,17 +7,17 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
-const router = require('./router/index')
+const router = require('./router/index.js')
 
 const PORT = process.env.PORT || 5000;
 
 // create an example of the app
 const app = express()
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-app.use('./api', router);
+app.use('/api', router);
 
 // start the app
 const start =  async() => {
