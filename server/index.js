d45a86d945987acd2932ac3a4e682683a775e731
SDen4 <asw325@yandex.ru>
2021-08-15 12:06:13 +0500

    start server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+// import needed libs
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const PORT = 5000;
+
+// create an example of the app
+const app = express()
+
+// start the app
+const start =  async() => {
+  try {
+    app.listen(PORT, () => console.log(`Yahoo! Server started on port: ${PORT}.`))
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// 
+start();
