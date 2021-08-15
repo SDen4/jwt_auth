0453bd445a9f80947831f0383e63196be7047e83
SDen4 <asw325@yandex.ru>
2021-08-15 17:26:56 +0500

    services

---
+++ server/service/mail-service.js
@@ -0,0 +1,17 @@
+const nodemailer = require('nodemailer');
+
+class MailService {
+  // constructor() {
+  //   this.transporter = nodemailer.createTransport({
+  //     host:
+  //     port:
+  //     secure: false,
+  //     auth: {
+
+  //     }
+  //   });
+  // }
+  async sendActivationMail(to, link) {};
+}
+
+module.exports = new MailService();
---
+++ server/service/user-service.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
+const mailService = require('./mail-service');
+const tokenService = require('./token-service');
+const UserModel = require('../models/user-model');
+const UserDto = require('../dtos/user-dto');
+
+class UserService {
+  async registration(email, password){
+    const candidate = await UserModel.findOne({email})
+    if (candidate) {
+      throw new Error(`Пользователь с email ${email} уже существует!`)
+    };
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = uuid.v4();
+
+    const user = await UserModel.create({email, password: hashPassword, activationLink});
+    await mailService.sendActivationMail(email, activationLink);
+
+    // userDto - user without password
+    const userDto = new UserDto(user); // id, email, isActivated
+    const tokens = tokenService.generateTokens({...userDto});
+
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {...tokens, user: userDto}
+  }
+}
+
+module.exports = new UserService();
