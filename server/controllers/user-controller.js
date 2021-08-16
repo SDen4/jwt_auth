0cafee4eab0c5a407a59587d54e0cd4814947c7a
SDen4 <asw325@yandex.ru>
2021-08-16 21:00:51 +0500

    activation

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -3,54 +3,50 @@ const userService = require('../service/user-service');
 class UserController {
   async registration(req, res, next) {
     try {
-      const {email, password} = req.body;
+      const { email, password } = req.body;
       const userData = await userService.registration(email, password);
-      res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 100, httpOnly: true})
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 100,
+        httpOnly: true,
+      });
       return res.json(userData);
     } catch (error) {
       console.log(error);
     }
-  };
+  }
 
   async login(req, res, next) {
     try {
-      
-    } catch (error) {
-      
-    }
-  };
+    } catch (error) {}
+  }
 
   async logout(req, res, next) {
     try {
-      
-    } catch (error) {
-      
-    }
-  };
+    } catch (error) {}
+  }
 
   async activate(req, res, next) {
     try {
-      
+      const activationLink = req.params.link;
+      await userService.activate(activationLink);
+      return res.redirect(process.env.CLIENT_URL);
     } catch (error) {
-      
+      console.log(error);
     }
-  };
+  }
 
   async refresh(req, res, next) {
     try {
-      
-    } catch (error) {
-      
-    }
-  };
+    } catch (error) {}
+  }
 
   async getUsers(req, res, next) {
     try {
-      res.json(['123', '456'])
+      res.json(['123', '456']);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  };
+  }
 }
 
 module.exports = new UserController();
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -7,24 +7,40 @@ const UserModel = require('../models/user-model');
 const UserDto = require('../dtos/user-dto');
 
 class UserService {
-  async registration(email, password){
-    const candidate = await UserModel.findOne({email})
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
     if (candidate) {
-      throw new Error(`Пользователь с email ${email} уже существует!`)
-    };
+      throw new Error(`Пользователь с email ${email} уже существует!`);
+    }
     const hashPassword = await bcrypt.hash(password, 3);
     const activationLink = uuid.v4();
 
-    const user = await UserModel.create({email, password: hashPassword, activationLink});
-    await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
+    const user = await UserModel.create({
+      email,
+      password: hashPassword,
+      activationLink,
+    });
+    await mailService.sendActivationMail(
+      email,
+      `${process.env.API_URL}/api/activate/${activationLink}`,
+    );
 
     // userDto - user without password
     const userDto = new UserDto(user); // id, email, isActivated
-    const tokens = tokenService.generateTokens({...userDto});
+    const tokens = tokenService.generateTokens({ ...userDto });
 
     await tokenService.saveToken(userDto.id, tokens.refreshToken);
 
-    return {...tokens, user: userDto}
+    return { ...tokens, user: userDto };
+  }
+
+  async activate(activationLink) {
+    const user = await UserModel.findOne({ activationLink });
+    if (!user) {
+      throw new Error('Некорректная ссылка активации');
+    }
+    user.isActivated = true;
+    await user.save();
   }
 }
 
